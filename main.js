0766b2ca03a036fa9917bd717bd0a2bf60a7348c
guilherme <106540089+guilherme21577@users.noreply.github.com>
2024-04-02 16:23:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const botoes=document.querySelectorAll(".botao");
-
+const textos=document.querySelectorAll(".aba-conteudo");
+const contadores=document.querySelectorAll(".contador");
+const tempoobjetivo1=new Date("2024-04-30");
+contadores[0].textContent=tempoobjetivo1;
 for(let i=0; i <botoes.length;i++) {
     botoes[i].onclick= function()  {
         for(let j=0; j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
-        botoes[i].classList.add("ativo");
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
     }
 }
